9d0a8e8413b23c337456b8a3a32b17f45c2f6fbd
yuri <yuri@shine.com>
2025-06-04 13:49:00 +0900

    [feature] p28

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,21 +1,36 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GameTest {
 
+    private Game game;
+
+    @BeforeEach
+    void setUp() {
+        game = new Game();
+    }
+
     @Test
     void createGame() {
-        Game game = new Game();
+
         assertNotNull(game);
     }
 
     @Test
     void throwExceptionWhenInputIsNull() {
         assertThrows(IllegalArgumentException.class,()->{
-            Game game=new Game();
+
             game.guess(null);
         });
 
+    }
+
+    @Test
+    void throwExceptionWhenInputLengthisUnmatched() {
+        assertThrows(IllegalArgumentException.class,()->{
+            game.guess("12");
+        });
     }
 }
